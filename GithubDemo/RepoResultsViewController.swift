edd5838e5a66d0ba7fa83fb5d256aa64e5f07dbe
Bianca Curutan <bmcurutan@gmail.com>
2016-10-15 23:30:00 -0700

    Milestone 3 iso

--- GithubDemo/RepoResultsViewController.swift
+++ GithubDemo/RepoResultsViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import MBProgressHUD
 
 // Main ViewController
-class RepoResultsViewController: UIViewController {
-
+class RepoResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var repoTableView: UITableView!
+    
     var searchBar: UISearchBar!
     var searchSettings = GithubRepoSearchSettings()
 
@@ -39,17 +41,45 @@ class RepoResultsViewController: UIViewController {
 
         // Perform request to GitHub API to get the list of repositories
         GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
-
+            self.repos = newRepos
+            
             // Print the returned repositories to the output window
             for repo in newRepos {
                 print(repo)
-            }   
+                //    self.repos.append(repo)
+            }
+            
+            self.repoTableView.reloadData()
 
             MBProgressHUD.hide(for: self.view, animated: true)
             }, error: { (error) -> Void in
                 print(error)
         })
     }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let repos = self.repos {
+            return repos.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let repo = self.repos[indexPath.row]
+        let cell = self.repoTableView.dequeueReusableCell(withIdentifier: "repoCell") as! RepoTableViewCell
+        
+        //cell.avatarImageView
+        cell.nameLabel.text = repo.name! as String
+        cell.ownerLabel.text = repo.ownerHandle! as String
+        cell.starsLabel.text = String(describing: repo.stars!)
+        cell.forksLabel.text = String(describing: repo.forks!)
+        cell.descriptionLabel.text = repo.repoDescription
+        
+        return cell
+    }
+
 }
 
 // SearchBar methods
@@ -76,3 +106,4 @@ extension RepoResultsViewController: UISearchBarDelegate {
         doSearch()
     }
 }
+
---
+++ GithubDemo/RepoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RepoTableViewCell.swift
+//  GithubDemo
+//
+//  Created by Bianca Curutan on 10/15/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+}
